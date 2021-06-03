c73d441920b5675ebcf43640cf1d2a6eddcea3de
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-06-03 09:18:37 -0600

    getall, get single, delete suggestion completed

--- routes/suggestions/controller/suggestionController.js
+++ routes/suggestions/controller/suggestionController.js
@@ -2,16 +2,35 @@ const Suggestion = require("../model/Suggestion");
 
 async function getAllSuggestions (req, res) {
     try {
-        let getAll = await Suggestion.find({})
-        res.json({ message: "success", data: getAll })
+        let getAllSuggest = await Suggestion.find({})
+        res.json({ message: "success", data: getAllSuggest })
     } catch (e) {
         res.status(500).json({ message: "failure", error: e.message })
     }
 }
-async function getSingleSuggestion (req, res) {};
+
+async function getSingleSuggestion (req, res) {
+    try {
+        let findOne = await Suggestion.findById(req.params.id)
+        res.json({message:"success", findOne}) 
+    } catch (error) {
+        res.status(500).json({message:"error", error:error.message})
+    }
+};
+
 async function createSuggestion (req, res) {};
+
 async function updateSuggestion (req, res) {};
-async function deleteSuggestion (req, res) {};
+
+async function deleteSuggestion (req, res) {
+    try {
+        console.log('path hit')
+        let delSuggestion = await Suggestion.findByIdAndRemove(req.params.id)
+        res.json({message:"success", delSuggestion})
+    } catch (error) {
+        res.status(500).json({message:'error', error:error.message})
+    }
+};
 
 
 
